712891c5263cad5c13e8e6700636dbe851e11007
Andres Echeverria <andres@brandpro.cl>
2022-09-08 18:34:39 -0300

    bootstrap ok

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import logoVertical from './logoVertical.svg';
-import logoSello from './logoSello.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <img src={logoVertical} alt="" />
-      <h1>Lorem ipsum dolor sit amet.</h1>
-      <h2>Lorem ipsum dolor sit amet.</h2>
-      <h3>Lorem ipsum dolor sit amet.</h3>
-      <h4>Lorem ipsum dolor sit amet.</h4> 
-      <p>Lorem ipsum dolor sit amet.</p>
-      <img src={logo} alt="" />
-      <img src={logoSello} alt="" />
 
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
 
   );
 }
