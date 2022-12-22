b114dcae8245a604d629957a7145211326e9e428
Andres Echeverria <andres@brandpro.cl>
2022-12-22 11:01:49 -0300

    testing build

---
+++ build/precache-manifest.58875a2f3b86e9deac5ebb8bff49c39a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "37f3334e932f564788a6",
+    "url": "/static/css/main.1682b399.chunk.css"
+  },
+  {
+    "revision": "37f3334e932f564788a6",
+    "url": "/static/js/main.37f3334e.chunk.js"
+  },
+  {
+    "revision": "dd02a13f5f447c639dd3",
+    "url": "/static/js/1.dd02a13f.chunk.js"
+  },
+  {
+    "revision": "56864587d6671f07ce53",
+    "url": "/static/js/2.56864587.chunk.js"
+  },
+  {
+    "revision": "ca941f47c15d629b2007",
+    "url": "/static/js/runtime~main.ca941f47.js"
+  },
+  {
+    "revision": "1333a54a07cffeb82317cd2297c0dcb9",
+    "url": "/static/media/logo.1333a54a.svg"
+  },
+  {
+    "revision": "31ffaafe83758df450090c1c283b79b4",
+    "url": "/static/media/ilustracionHome.31ffaafe.svg"
+  },
+  {
+    "revision": "18cda957e3b58ff61045a7a22a8b5f86",
+    "url": "/static/media/ilustracionHomeB.18cda957.svg"
+  },
+  {
+    "revision": "cb278924984ef7425911f5f515f413a4",
+    "url": "/static/media/logoVertical-blanco.cb278924.png"
+  },
+  {
+    "revision": "a064a3b03551b981284515891e4d280c",
+    "url": "/static/media/membreteNotaria.a064a3b0.png"
+  },
+  {
+    "revision": "a31b88732fc85757a380910b08118b7d",
+    "url": "/index.html"
+  }
+];
